b63151f77f8f1b2bf546be332b26cffaa4eed512
savisulo <suvi.j.salo@gmail.com>
2023-04-03 14:48:40 +0300

    osan 3 tehtävät (ei 3.22)

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const Contact = require('./models/contact')
 var morgan = require('morgan')
 const app = express()
 
@@ -7,94 +9,117 @@ morgan.token('body', function getResponse (req) {
     return JSON.stringify(req.body)
   })
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+  
+    next(error)
+}
+
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
-app.use(express.static('build'))
-
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345",
-      },
-      {
-        id: 4,
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"
-      }
-]
-
-const number_of_persons = persons.length
-const date = new Date();
 
 app.get('/info', (request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' })
-    response.end(`Phonebook has info for ${number_of_persons} people\n${date}`)
+    Contact.countDocuments({}).then(count => {
+        const date = new Date();
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.end(`Phonebook has info for ${count} people\n${date}`)
+    })
+    .catch(error => {
+        console.error(error);
+        response.status(500).send('Error retrieving contacts');
+    });
 })
 
-const generateId = (max) => {
-    return Math.floor(Math.random() * max);
-}
-
-app.post('/api/persons', (request, response) => {
+app.post('/api/contacts', (request, response, next) => {
     const body = request.body
-    const name = persons.some(person => person.name === body.name)
+    
+    Contact.findOne({ name: body.name })
+    .then(existingContact => {
+        if (existingContact) {
+            const id = existingContact._id;
 
-    if (!body.name || !body.number) {
-        return response.status(400).json({ 
-          error: 'name or number missing' 
-        })
-    } else if (name) {
-        return response.status(400).json({ 
-            error: 'name already exists in phonebook' 
-        })
-    }
+            app.put(`/api/contacts/${id}`, (request, response, next) => {
+                const body = request.body;
 
-    const person = {
-        id: generateId(10000),
+                const contact = {
+                    name: body.name,
+                    number: body.number,
+                };
+
+                Contact.findByIdAndUpdate(id, contact, { new: true })
+                    .then(updatedContact => {
+                        response.json(updatedContact);
+                    })
+                    .catch(error => next(error));
+            });
+
+            response.redirect(`PUT /api/contacts/${id}`);
+        } else {
+            const contact = new Contact({
+                name: body.name,
+                number: body.number,
+            });
+
+            contact.save().then(savedContact => {
+                response.json(savedContact)
+            })
+            .catch(error => next(error));
+        }
+    })
+    .catch(error => next(error));
+});
+
+app.put('/api/contacts/:id', (request, response, next) => {
+    const body = request.body;
+
+    const contact = {
         name: body.name,
         number: body.number,
-    }
-  
-    persons = persons.concat(person)
-  
-    response.json(person)
-})
-  
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
-})
+    };
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-      } else {
-        response.status(404).end()
-    }
+    Contact.findByIdAndUpdate(request.params.id, contact, { new: true })
+        .then(updatedContact => {
+            response.json(updatedContact);
+        })
+        .catch(error => next(error));
+});
+  
+app.get('/api/contacts', (request, response) => {
+    Contact.find({}).then(contacts => {
+        response.json(contacts)
+    })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+app.get('/api/contacts/:id', (request, response, next) => {
+    Contact.findById(request.params.id).then(contact => {
+        if (contact) {
+            response.json(contact)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
 })
 
-const PORT = process.env.PORT || 3003
+app.delete('/api/contacts/:id', (request, response, next) => {
+    Contact.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ models/contact.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const contactSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        minlength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        minlength: 8,
+        validate: {
+            validator: function(v) {
+              return /\d{2,3}-\d{5,}/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number. The number must start with two or three digits followed by a line and then at least five digits.`
+          },
+        required: true
+    },
+})
+  
+contactSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Contact', contactSchema)
